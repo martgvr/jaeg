4cfb852835d80d76aeb56e610eb0f62dcadbffdf
Martin Guevara <martin_91@live.com.ar>
2023-01-31 18:14:26 -0300

    Improving variable replacement

--- generator.js
+++ generator.js
@@ -5,9 +5,8 @@ clearFile()
 const config = {
     PORT: 3001,
     mode: 'cluster',
-    layers: ['carts', 'products', 'users']
+    layers: ['carts', 'products', 'users'],
 }
-
 
 const fileContent = await readFile()
 
@@ -17,27 +16,25 @@ for (const line of fileContent) {
 
     if (lineMatch !== null) {
         let varsToReplace = []
-        for (const variable of lineMatch) varsToReplace.push(variable.replace(regex, '$1'))
+
+        for (const variable of lineMatch) {
+            varsToReplace.push(variable.replace(regex, '$1'))
+        }
 
         let previousLine, newLine
+
         for (let i = 0; i < varsToReplace.length; i++) {
-            if (i === 0) {
-                previousLine = line
-                for (const key in config) {
-                    if (key === varsToReplace[i]) {
-                        newLine = previousLine.replace('[[' + key + ']]', config[key])
-                    }
-                }
-            } else {
-                previousLine = newLine
-                for (const key in config) {
-                    if (key === varsToReplace[i]) {
-                        newLine = previousLine.replace('[[' + key + ']]', config[key])
-                    }
+            previousLine = i === 0 ? line : newLine
+
+            for (const key in config) {
+                if (key === varsToReplace[i]) {
+                    newLine = previousLine.replace('[[' + key + ']]', config[key])
                 }
             }
         }
+
         await writeLine(newLine)
+
     } else {
         await writeLine(line)
     }
