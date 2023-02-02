fa355b0263a5e3047f4f98b759c3cb0e020545d8
Martin Guevara <martin_91@live.com.ar>
2023-02-02 18:50:21 -0300

    directory and files cloning development

--- utils/structureCreator.js
+++ utils/structureCreator.js
@@ -1,19 +1,37 @@
 import fs from 'fs'
 import path from 'path'
 
-const rootPath = '../'
+const rootPath = '../templates'
 
-export function getFiles(rootPath) {
+function getFiles(rootPath) {
     const isFile = (input) => { return fs.lstatSync(input).isFile() }
     const files = fs.readdirSync(rootPath).map(fileName => { return path.join(rootPath, fileName) }).filter(isFile)
     return files
 }
 
-export function getDirectories(rootPath) {
+function getDirectories(rootPath) {
     const isDirectory = (input) => { return fs.lstatSync(input).isDirectory() }
     const directories = fs.readdirSync(rootPath).map(fileName => { return path.join(rootPath, fileName) }).filter(isDirectory)
     return directories
 }
 
-console.log(getFiles(rootPath))
-console.log(getDirectories(rootPath))
+const files = getFiles(rootPath)
+const directories = getDirectories(rootPath)
+
+export function cloneTemplate() {
+    if (!fs.existsSync('../generated')) {
+        fs.mkdir('../generated', (error) => error && console.log('Error al crear directorio base', error))
+    }
+
+    for (const file of files) {
+        try {
+            fs.writeFileSync('../generated/' + path.basename(file), '');
+          } catch (err) {
+            console.error(err);
+          }
+    }
+}
+
+cloneTemplate()
+
+
